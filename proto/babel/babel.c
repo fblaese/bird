fblaese/bird

commit 3e7e4a71868bc519aacc0eb785471b46fc345a5c

Babel: Fix missing modulo comparison of seqnos
Juliusz noticed there were a couple of places we were doing straight
inequality comparisons of seqnos in Babel. This is wrong because seqnos can
wrap: so we need to use the modulo-64k comparison function for these cases
as well.

Introduce a strict-inequality version of the modulo-comparison for this
purpose.

proto/babel/babel.c
@@ -49,6 +49,10 @@
 static inline int ge_mod64k(uint a, uint b)
 { return (u16)(a - b) < 0x8000; }
 
+/* Strict inequality version of the above */
+static inline int gt_mod64k(uint a, uint b)
+{ return ge_mod64k(a, b) && a != b; }
+
 static void babel_expire_requests(struct babel_proto *p, struct babel_entry *e);
 static void babel_select_route(struct babel_proto *p, struct babel_entry *e, struct babel_route *mod);
 static inline void babel_announce_retraction(struct babel_proto *p, struct babel_entry *e);
@@ -559,7 +563,7 @@ babel_is_feasible(struct babel_source *s, u16 seqno, u16 metric)
 {
   return !s ||
     (metric == BABEL_INFINITY) ||
-    (seqno > s->seqno) ||
+    gt_mod64k(seqno, s->seqno) ||
     ((seqno == s->seqno) && (metric < s->metric));
 }
 
@@ -1013,7 +1017,7 @@ babel_send_update_(struct babel_iface *ifa, btime changed, struct fib *rtable)
       struct babel_source *s = babel_get_source(p, e, e->router_id);
       s->expires = current_time() + BABEL_GARBAGE_INTERVAL;
 
-      if ((msg.update.seqno > s->seqno) ||
+      if (gt_mod64k(msg.update.seqno, s->seqno) ||
 	  ((msg.update.seqno == s->seqno) && (msg.update.metric < s->metric)))
       {
 	s->seqno = msg.update.seqno;

commit 8472a4616726f80d9cfa1a35314ebe30786897ce

Babel: fix seqno wrapping on seqno request
The Babel seqno wraps around when reaching its maximum value
(UINT16_MAX). When comparing seqnos, this has to be taken into account.
Therefore, plain number comparisons do not work.

In a previous attempt to fix the wrapping behavior, one particular
comparison was missed. This causes the seqno of originated babel routes
to not wrap, but remain at UINT16_MAX indefinitely, resulting in slow
route propagation on topology changes.

Make use of the previously introduced gt_mod64k macro to compare seqnos
correctly.

Fixes: 3e7e4a71868bc519aacc0eb785471b46fc345a5c

Signed-off-by: Fabian Bläse <fabian@blaese.de>

proto/babel/babel.c
@@ -1015,7 +1015,7 @@ babel_send_update_(struct babel_iface *ifa, btime changed, struct fib *rtable)
 
     /* Our own seqno might have changed, in which case we update the routes we
        originate. */
-    if ((e->router_id == p->router_id) && (e->seqno < p->update_seqno))
+    if ((e->router_id == p->router_id) && (gt_mod64k(p->update_seqno, e->seqno))
     {
       e->seqno = p->update_seqno;
       e->updated = current_time();